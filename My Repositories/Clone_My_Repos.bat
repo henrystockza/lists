edf4873421ad10195c24aa651181f88986ef504d
HenryStocksa <hstockza@gmail.com>
2017-12-05 21:06:38 +0200

    updates

---
+++ My Repositories/Clone_My_Repos.bat
@@ -0,0 +1,4 @@
+git clone git@github.com:henrystockza/lists.git
+git clone git@github.com:henrystockza/linux.git
+git clone git@github.com:henrystockza/python.git
+git clone git@github.com:henrystockza/powershell.git
